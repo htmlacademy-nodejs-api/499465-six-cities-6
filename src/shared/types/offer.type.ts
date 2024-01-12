9ddd03cd24b315af8126851d8faf596a5f0276e7
Anastasia Stepannikova <asya.ya2016@gmail.com>
2024-01-12 21:35:46 +0500

    Добавляет типы для импортируемых сущностей

---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,24 @@
+import { City } from './city-type.enum.js';
+import { HousingType } from './housing-type.enum.js';
+import { Conveniency } from './conveniency-type.enum.js';
+import { Coordinate } from './coordinate-type.enum.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: City;
+  preview: string,
+  images: string[];
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  housingType: HousingType;
+  roomsQuantity: number;
+  guestsQuantity: number;
+  price: number;
+  conveniencies: Conveniency[];
+  author: string;
+  commentsQuantity: number,
+  coordinates: Coordinate;
+}
